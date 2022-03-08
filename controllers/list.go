3b849a76bd57371d0167eaad9ef02dced713a12f
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-03-08 21:25:24 +0200

    soft delete list

--- controllers/list.go
+++ controllers/list.go
@@ -93,7 +93,9 @@ func DeleteList(c *gin.Context) {
 		return
 	}
 
-	result := models.DB.Unscoped().Delete(&models.List{}, id)
+	// result := models.DB.Unscoped().Delete(&models.List{}, id)
+	//the following soft deletes the list
+	result := models.DB.Select("Items").Delete(&models.List{}, id)
 
 	if result.Error != nil {
 		c.JSON(404, gin.H{
