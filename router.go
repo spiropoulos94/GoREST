913832cd302fbc0a01276c43c028fa0c017b3508
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-01 04:25:08 +0300

    test

--- router.go
+++ router.go
@@ -15,20 +15,31 @@ func routerStart() {
 
 	//groupare sto "/api" olo to user kai to list
 
-	user := router.Group("/user")
-	list := router.Group("/list")
+	api := router.Group("/api")
+	{
+		api.GET("user/", controllers.GetUsers)
+		api.GET("user/:id", controllers.FindUser)
+		api.POST("user/", controllers.CreateUser)
+		api.DELETE("user/:id", controllers.DeleteUser)
 
-	// User Routes
-	user.GET("/", controllers.GetUsers)
-	user.GET("/:id", controllers.FindUser)
-	user.POST("/", controllers.CreateUser)
-	user.DELETE("/:id", controllers.DeleteUser)
-	// List Routes
-	list.GET("/", controllers.GetLists)
-	list.POST("/", controllers.CreateList)
-	list.GET("/:id", controllers.FindList)
-	list.DELETE("/:id", controllers.DeleteList)
-	list.PUT("/:id", controllers.UpdateList)
+		api.GET("list/", controllers.GetLists)
+		api.POST("list/", controllers.CreateList)
+		api.GET("list/:id", controllers.FindList)
+		api.DELETE("list/:id", controllers.DeleteList)
+		api.PUT("list/:id", controllers.UpdateList)
+	}
+
+	// // User Routes
+	// user.GET("/", controllers.GetUsers)
+	// user.GET("/:id", controllers.FindUser)
+	// user.POST("/", controllers.CreateUser)
+	// user.DELETE("/:id", controllers.DeleteUser)
+	// // List Routes
+	// list.GET("/", controllers.GetLists)
+	// list.POST("/", controllers.CreateList)
+	// list.GET("/:id", controllers.FindList)
+	// list.DELETE("/:id", controllers.DeleteList)
+	// list.PUT("/:id", controllers.UpdateList)
 
 	// Auth routes
 	router.POST("/signup", controllers.Signup)
