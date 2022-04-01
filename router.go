c1fd4b0a01fc7ed2f7bb4bc7f00c632578ec59d5
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-01 19:20:01 +0300

    GROUPED ALL /API ROUTES

--- router.go
+++ router.go
@@ -15,31 +15,27 @@ func routerStart() {
 
 	//groupare sto "/api" olo to user kai to list
 
-	api := router.Group("/api")
+	api := router.Group("/api", CheckHeaderForJWT())
 	{
-		api.GET("user/", controllers.GetUsers)
-		api.GET("user/:id", controllers.FindUser)
-		api.POST("user/", controllers.CreateUser)
-		api.DELETE("user/:id", controllers.DeleteUser)
+		userGroup := api.Group("/user")
+		{
+			// user group handlers
+			userGroup.GET("/", controllers.GetUsers)
+			userGroup.GET("/:id", controllers.FindUser)
+			userGroup.POST("/", controllers.CreateUser)
+			userGroup.DELETE("/:id", controllers.DeleteUser)
+		}
 
-		api.GET("list/", controllers.GetLists)
-		api.POST("list/", controllers.CreateList)
-		api.GET("list/:id", controllers.FindList)
-		api.DELETE("list/:id", controllers.DeleteList)
-		api.PUT("list/:id", controllers.UpdateList)
+		listGroup := api.Group("/list")
+		{
+			// todo group handlers
+			listGroup.GET("/", controllers.GetLists)
+			listGroup.POST("/", controllers.CreateList)
+			listGroup.GET("/:id", controllers.FindList)
+			listGroup.DELETE("/:id", controllers.DeleteList)
+			listGroup.PUT("/:id", controllers.UpdateList)
+		}
 	}
-
-	// // User Routes
-	// user.GET("/", controllers.GetUsers)
-	// user.GET("/:id", controllers.FindUser)
-	// user.POST("/", controllers.CreateUser)
-	// user.DELETE("/:id", controllers.DeleteUser)
-	// // List Routes
-	// list.GET("/", controllers.GetLists)
-	// list.POST("/", controllers.CreateList)
-	// list.GET("/:id", controllers.FindList)
-	// list.DELETE("/:id", controllers.DeleteList)
-	// list.PUT("/:id", controllers.UpdateList)
 
 	// Auth routes
 	router.POST("/signup", controllers.Signup)
